7473e2ac66f2657c745de5b28cdcbe085e3c67f6
Ashutosh Gautam <aashutoshgautam2611@gmail.com>
2020-07-15 10:56:11 +0530

    Updates

---
+++ precache-manifest.84cb801c4b191533fdc2a5d3b4bfb9d4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb641e64be2b9b4a8a081133f44d75d2",
+    "url": "/to-do-app/index.html"
+  },
+  {
+    "revision": "4247a49a134cee5aaa61",
+    "url": "/to-do-app/static/css/2.eaa14dea.chunk.css"
+  },
+  {
+    "revision": "0041793cdf2a5fa369dd",
+    "url": "/to-do-app/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "4247a49a134cee5aaa61",
+    "url": "/to-do-app/static/js/2.ae7cb46e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/to-do-app/static/js/2.ae7cb46e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0041793cdf2a5fa369dd",
+    "url": "/to-do-app/static/js/main.819b5964.chunk.js"
+  },
+  {
+    "revision": "4385e1bd226fdb470057",
+    "url": "/to-do-app/static/js/runtime-main.1f8cf036.js"
+  }
+]);
